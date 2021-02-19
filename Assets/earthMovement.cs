6c6c3c5ef440be4f3b50e634dc78789516b6af80
Felix.A <kiwibirdi@hotmail.com>
2021-02-19 13:54:14 +0100

    first test for simon tweak

---
+++ Assets/earthMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earthMovement : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+    }
+}
