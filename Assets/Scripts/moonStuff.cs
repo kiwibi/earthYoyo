591351ba37780dddcb0e97cba5ce5ea3aace2da1
Felix.A <kiwibirdi@hotmail.com>
2021-02-23 14:45:11 +0100

    tmp

--- Assets/Scripts/moonStuff.cs
+++ Assets/Scripts/moonStuff.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class moonStuff : MonoBehaviour
 {
     public Transform earth_;
-
+    public float pushbackPower_;
     private LineRenderer line_;
     void Start()
     {
@@ -17,5 +17,25 @@ public class moonStuff : MonoBehaviour
     {
         line_.SetPosition(0, transform.position);
         line_.SetPosition(1, earth_.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(!col.CompareTag("Earth"))
+        {
+            Debug.Log("not earth");
+            Vector3 dir = earth_.position - transform.position;
+            Vector3 force = dir.normalized * pushbackPower_;
+            GetComponent<Rigidbody2D>().AddForce(force);
+            Destroy(col.gameObject);
+        }
+        else
+        {
+            Debug.Log("=)");
+            Vector3 dir = earth_.position - transform.position;
+            Vector3 force = dir.normalized * pushbackPower_;
+            GetComponent<Rigidbody2D>().AddForce(force);
+        }
+
     }
 }
